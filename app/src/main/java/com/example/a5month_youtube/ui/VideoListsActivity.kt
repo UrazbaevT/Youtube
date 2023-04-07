5716dc2893f76e51888b4aa069e1d08b3b502601
UrazbaevT <utilek990@gmail.com>
2023-04-07 12:45:02 +0300

    added code of hw1 + corrected

--- app/src/main/java/com/example/a5month_youtube/ui/VideoListsActivity.kt
+++ app/src/main/java/com/example/a5month_youtube/ui/VideoListsActivity.kt
@@ -1,13 +1,18 @@
 package com.example.a5month_youtube.ui
 
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.a5month_youtube.base.BaseActivity
 import com.example.a5month_youtube.base.BaseViewModel
 import com.example.a5month_youtube.databinding.ActivityVideoListBinding
 
-class VideoListsActivity(override val viewModel: BaseViewModel) : BaseActivity<ActivityVideoListBinding, BaseViewModel>() {
+class VideoListsActivity : BaseActivity<ActivityVideoListBinding, BaseViewModel>() {
     override fun inflateViewBinding(): ActivityVideoListBinding {
         return ActivityVideoListBinding.inflate(layoutInflater)
+    }
+
+    override val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
     }
 
     override fun initListener() {
